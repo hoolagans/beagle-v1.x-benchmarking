128620c6a98566b591ab3d8b51824fa3240ef2da
Ilya @ Kili <ilya.basin@gmail.com>
2025-10-09 16:01:41 -0400

    progress

--- Beagle/Run/MLSetups/XPowY.cs
+++ Beagle/Run/MLSetups/XPowY.cs
@@ -3,18 +3,20 @@ using BeagleLib.Util;
 
 namespace Run.MLSetups;
 
-public class AreaOfCircle : MLSetup
+public class XPowY : MLSetup
 {
     #region Overrides
     public override (float[], float) GetNextInputsAndCorrectOutput(float[] inputs)
     {
-        var r = Rnd.Random.NextSingle() * 1000;
-        inputs[0] = r;
-        return (inputs, r*r*MathF.PI);
+        var x = Rnd.Random.NextSingle() * 10;
+        var y = Rnd.Random.NextSingle() * 5;
+        inputs[0] = x;
+        inputs[1] = y;
+        return (inputs, MathF.Pow(x, y));
     }
     public override string[] GetInputLabels()
     {
-        return ["r"];
+        return ["x", "y"];
     }
 
     public override int TargetColonySize(int generation)
