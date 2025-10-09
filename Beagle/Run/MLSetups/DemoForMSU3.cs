0968e3aee5ae9c2ccf5f47f12e1d1e5487e45acb
Ilya @ Dell Precision <ilya.basin@noblis.org>
2025-10-09 10:17:31 -0400

    progress

--- Beagle/Run/MLSetups/DemoForMSU3.cs
+++ Beagle/Run/MLSetups/DemoForMSU3.cs
@@ -14,31 +14,33 @@ public class DemoForMSU3 : MLSetup
         var m2 = 1 + Rnd.Random.NextSingle();
         var x1 = 3 + Rnd.Random.NextSingle();
         var x2 = 1 + Rnd.Random.NextSingle();
-        var y1 = 3 + Rnd.Random.NextSingle();
-        var y2 = 1 + Rnd.Random.NextSingle();
-        var z1 = 3 + Rnd.Random.NextSingle();
-        var z2 = 1 + Rnd.Random.NextSingle();
+        //var y1 = 3 + Rnd.Random.NextSingle();
+        //var y2 = 1 + Rnd.Random.NextSingle();
+        //var z1 = 3 + Rnd.Random.NextSingle();
+        //var z2 = 1 + Rnd.Random.NextSingle();
 
         inputs[0] = g;
         inputs[1] = m1;
         inputs[2] = m2;
         inputs[3] = x1;
         inputs[4] = x2;
-        inputs[5] = y1;
-        inputs[6] = y2;
-        inputs[7] = z1;
-        inputs[8] = z2;
+        //inputs[5] = y1;
+        //inputs[6] = y2;
+        //inputs[7] = z1;
+        //inputs[8] = z2;
 
         var dx = x2 - x1;
-        var dy = y2 - y1;
-        var dz = z2 - z1;
+        //var dy = y2 - y1;
+        //var dz = z2 - z1;
 
-        var result = (g * m1 * m2)/(dx*dx + dy*dy + dz*dz);
+        //var result = (g * m1 * m2)/(dx*dx + dy*dy + dz*dz);
+        var result = (g * m1 * m2) / (dx * dx);
         return (inputs, result);
     }
     public override string[] GetInputLabels()
     {
-        return ["G", "m1", "m2", "x1", "x2", "y1", "y2", "z1", "z2"];
+        //return ["G", "m1", "m2", "x1", "x2", "y1", "y2", "z1", "z2"];
+        return ["G", "m1", "m2", "x1", "x2"];
     }
 
     public override int TargetColonySize(int generation)
@@ -49,7 +51,7 @@ public class DemoForMSU3 : MLSetup
 
     protected override int ScriptLengthTaxRateInternal => BConfig.MaxScore * (int)ExperimentsPerGeneration / 300;
 
-    public override long TotalBirthsToResetColonyIfNoProgress => 2_500_000_000;
+    public override long TotalBirthsToResetColonyIfNoProgress => 1_500_000_000;
 
     public override uint ExperimentsPerGeneration => 1024;
 
